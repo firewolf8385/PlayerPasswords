f9b1058d7e48a1f5503b8e9de1f9c56a864779a8
Colby <colbykrawczyk10@gmail.com>
2019-08-18 22:27:50 -0400

    Added Verified Players List
    This can be accessed with /playerpasswords verified

--- firewolf8385/playerpasswords/commands/PP.java
+++ firewolf8385/playerpasswords/commands/PP.java
@@ -1,10 +1,12 @@
 package firewolf8385.playerpasswords.commands;
 
+import firewolf8385.playerpasswords.PlayerPasswords;
 import firewolf8385.playerpasswords.SettingsManager;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class PP implements CommandExecutor
 {
@@ -49,6 +51,15 @@ public class PP implements CommandExecutor
                     sender.sendMessage(ChatColor.translateAlternateColorCodes('&', gold + "&l]" + gray +"&m---------------------------------------------------" + gold + "&l["));
                     break;
 
+                case "verified":
+                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&', gold + "&l]" + gray + "&m-----------------" + gold + "&lPlayerPasswords" + gray + "&m-----------------" + gold +"&l["));
+                    for(Player p : PlayerPasswords.verified)
+                    {
+                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "  " + gray + "» " + yellow + p.getName()));
+                    }
+                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&', gold + "&l]" + gray +"&m---------------------------------------------------" + gold + "&l["));
+                    break;
+
             }
         }
         else
@@ -56,6 +67,7 @@ public class PP implements CommandExecutor
             sender.sendMessage(ChatColor.translateAlternateColorCodes('&', gold + "&l]" + gray + "&m-----------------" + gold + "&lPlayerPasswords" + gray + "&m-----------------" + gold +"&l["));
             sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "  " + gray + "» " + gold + "/pp info " + gray + "- " + yellow + "Shows plugin info."));
             sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "  " + gray + "» " + gold + "&6/pp support " + gray + "- " + yellow + "Display's the support discord link."));
+            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "  " + gray + "» " + gold + "&6/pp verified " + gray + "- " + yellow + "List all verified players."));
             sender.sendMessage(ChatColor.translateAlternateColorCodes('&', gold + "&l]" + gray +"&m---------------------------------------------------" + gold + "&l["));
         }
 
