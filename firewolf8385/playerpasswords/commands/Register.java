d362a9abdbe701310ca288983943dfedb0c6d589
Colby <colbykrawczyk10@gmail.com>
2019-08-18 22:55:03 -0400

    Fixed Register.java
    Cleaned up code, and fixed a few bugs with /register.
    
    Bugs fixed:
        - NPE thrown when typing /register with no args
        - /register can be used when logged in.
        - /register can be used even when password was already set, defeating the purpose of a password.

--- firewolf8385/playerpasswords/commands/Register.java
+++ firewolf8385/playerpasswords/commands/Register.java
@@ -16,40 +16,55 @@ public class Register implements CommandExecutor
     {
         Player p = (Player) sender;
 
-        if(PlayerPasswords.verified.contains(p) || !(settings.getData().getBoolean("passwords." + p.getName() + ".enabled")))
+        boolean verified = PlayerPasswords.verified.contains(p);
+        boolean enabled = settings.getData().getBoolean("passwords." + p.getName() + ".enabled");
+
+        // If the player is already logged in, the command will end.
+        if(verified)
         {
-            if(args.length != 0)
+            p.sendMessage(ChatColor.translateAlternateColorCodes('&', settings.getConfig().getString("AlreadyLoggedIn")));
+            return true;
+        }
+
+        // If the player already set their password, the command will end.
+        if(enabled)
+        {
+            p.sendMessage(ChatColor.translateAlternateColorCodes('&', settings.getConfig().getString("AlreadyRegistered")));
+            return true;
+        }
+
+        // If the player did not enter a password, the command will end.
+        if(args.length == 0)
+        {
+            p.sendMessage(ChatColor.translateAlternateColorCodes('&', settings.getConfig().getString("RegisterUsage")));
+            return true;
+        }
+
+
+        int minimum = settings.getConfig().getInt("MinimumPasswordLength");
+        int maximum = settings.getConfig().getInt("MaximumPasswordLength");
+
+        if(args[0].length() >= minimum && args[0].length() <= maximum)
+        {
+            settings.getData().set("passwords." + p.getName() + ".password", args[0]);
+
+            p.sendMessage(ChatColor.translateAlternateColorCodes('&', settings.getConfig().getString("SetPasswordSuccessful")));
+            if(!(PlayerPasswords.verified.contains(p)))
             {
-                int minimum = settings.getConfig().getInt("MinimumPasswordLength");
-                int maximum = settings.getConfig().getInt("MaximumPasswordLength");
-
-                if(args[0].length() >= minimum && args[0].length() <= maximum)
-                {
-                    settings.getData().set("passwords." + p.getName() + ".password", args[0]);
-
-                    p.sendMessage(ChatColor.translateAlternateColorCodes('&', settings.getConfig().getString("SetPasswordSuccessful")));
-                    if(!(PlayerPasswords.verified.contains(p)))
-                    {
-                        PlayerPasswords.verified.add(p);
-                    }
-
-                    if(!(settings.getData().getBoolean("passwords." + p.getName() + ".enabled")))
-                    {
-                        settings.getData().set("passwords." + p.getName() + ".enabled", true);
-                    }
-
-                    settings.saveData();
-                    settings.reloadData();
-                }
-                else
-                {
-                    p.sendMessage(ChatColor.translateAlternateColorCodes('&', settings.getConfig().getString("OutOfBounds")));
-                }
+                PlayerPasswords.verified.add(p);
             }
-            else
+
+            if(!(settings.getData().getBoolean("passwords." + p.getName() + ".enabled")))
             {
-                p.sendMessage(ChatColor.translateAlternateColorCodes('&', settings.getConfig().getString("SetPasswordUsage")));
+                settings.getData().set("passwords." + p.getName() + ".enabled", true);
             }
+
+            settings.saveData();
+            settings.reloadData();
+        }
+        else
+        {
+            p.sendMessage(ChatColor.translateAlternateColorCodes('&', settings.getConfig().getString("OutOfBounds")));
         }
 
         return true;
