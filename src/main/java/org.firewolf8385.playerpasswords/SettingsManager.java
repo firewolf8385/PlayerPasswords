098747a1887314c93024c1b960ca19843eef64a3
colby <colbykrawczyk10@gmail.com>
2021-01-23 09:09:24 -0500

    Cleaned up SettingsManager.java

--- src/main/java/org.firewolf8385.playerpasswords/SettingsManager.java
+++ src/main/java/org.firewolf8385.playerpasswords/SettingsManager.java
@@ -6,50 +6,42 @@ import org.bukkit.plugin.Plugin;
 import java.io.File;
 import java.io.IOException;
 
-public class SettingsManager
-{
+/**
+ * Manages Configuration and Data storage files for the plugin.
+ */
+public class SettingsManager {
+    private FileConfiguration config;
+    private FileConfiguration data;
+    private File dataFile;
+    private static final SettingsManager instance = new SettingsManager();
 
     private SettingsManager() {}
-    static SettingsManager instance = new SettingsManager();
 
     /**
      * This allows us to access an instance of this class.
      */
-    public static SettingsManager getInstance()
-    {
+    public static SettingsManager getInstance() {
         return instance;
     }
-
-    Plugin pl;
-    FileConfiguration config;
-    File configFile;
-
-    FileConfiguration data;
-    File dataFile;
-
 
     /**
      * This allows us to set up the config file if it does not exist.
      * @param pl Instance of the Plugin
      */
-    public void setup(Plugin pl)
-    {
+    public void setup(Plugin pl) {
         config = pl.getConfig();
         config.options().copyDefaults(true);
-        configFile = new File(pl.getDataFolder(), "config.yml");
+        new File(pl.getDataFolder(), "config.yml");
         pl.saveDefaultConfig();
 
         dataFile = new File(pl.getDataFolder(), "data.yml");
         data = YamlConfiguration.loadConfiguration(dataFile);
 
-        if(!dataFile.exists())
-        {
-            try
-            {
+        if(!dataFile.exists()) {
+            try {
                 dataFile.createNewFile();
             }
-            catch(IOException e)
-            {
+            catch(IOException e) {
                 e.printStackTrace();
             }
         }
@@ -59,54 +51,26 @@ public class SettingsManager
      * Allows us to access the config file.
      * @return config file
      */
-    public FileConfiguration getConfig()
-    {
+    public FileConfiguration getConfig() {
         return config;
-    }
-
-    /**
-     * Allows us to save the config file after changes are made.
-     */
-    public void saveConfig()
-    {
-        try
-        {
-            config.save(configFile);
-        }
-        catch(IOException e)
-        {
-            e.printStackTrace();
-        }
-    }
-
-    /**
-     * This updates the config in case changes are made.
-     */
-    public void reloadConfig()
-    {
-        config = YamlConfiguration.loadConfiguration(configFile);
     }
 
     /**
      * Allows us to access the data file.
      * @return data file
      */
-    public FileConfiguration getData()
-    {
+    public FileConfiguration getData() {
         return data;
     }
 
     /**
      * Allows us to save the config file after changes are made.
      */
-    public void saveData()
-    {
-        try
-        {
+    public void saveData() {
+        try {
             data.save(dataFile);
         }
-        catch(IOException e)
-        {
+        catch(IOException e) {
             e.printStackTrace();
         }
     }
@@ -114,8 +78,7 @@ public class SettingsManager
     /**
      * This updates the data in case changes are made.
      */
-    public void reloadData()
-    {
+    public void reloadData() {
         data = YamlConfiguration.loadConfiguration(dataFile);
     }
 }
