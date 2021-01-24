f4508371bfa4b4ef4a38fe0ff411072463303059
Colby <colbykrawczyk10@gmail.com>
2021-01-24 00:30:28 -0500

    Cleaned up Login command

--- src/main/java/org.firewolf8385.playerpasswords/commands/Login.java
+++ src/main/java/org.firewolf8385.playerpasswords/commands/Login.java
@@ -10,15 +10,13 @@ import org.bukkit.entity.Player;
 import org.firewolf8385.playerpasswords.objects.PasswordPlayer;
 import org.firewolf8385.playerpasswords.utils.StringUtils;
 
-public class Login implements CommandExecutor
-{
-    SettingsManager settings = SettingsManager.getInstance();
+public class Login implements CommandExecutor {
+    private final SettingsManager settings = SettingsManager.getInstance();
 
-    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args)
-    {
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         // Exit if not a player.
-        if(!(sender instanceof Player))
-        {
+        if(!(sender instanceof Player)) {
+            Utils.chat(sender, settings.getConfig().getString("MustBePlayer"));
             return true;
         }
 
@@ -26,33 +24,27 @@ public class Login implements CommandExecutor
         String uuid = p.getUniqueId().toString();
         PasswordPlayer pl = PasswordPlayer.getPlayers().get(p.getUniqueId());
 
-        // If The Command Is Run Without Args, Show Error Message
-        if(args.length == 0)
-        {
-            Utils.chat(p, settings.getConfig().getString("LoginUsage"));
-            return true;
-        }
-
         // If the player is already logged in, they can't log in again.
-        if(pl.isVerified())
-        {
+        if(pl.isVerified()) {
             Utils.chat(p, settings.getConfig().getString("AlreadyLoggedIn"));
             return true;
         }
 
-        if(StringUtils.hash(args[0]) == (settings.getData().getInt("passwords." + uuid + ".password")))
-        {
+        // If The Command Is Run Without Args, Show Error Message
+        if(args.length == 0) {
+            Utils.chat(p, settings.getConfig().getString("LoginUsage"));
+            return true;
+        }
+
+        if(StringUtils.hash(args[0]) == (settings.getData().getInt("passwords." + uuid + ".password"))) {
             Utils.chat(p, settings.getConfig().getString("LogInSuccessful"));
             pl.setVerified(true);
         }
-        else
-        {
-            if(settings.getConfig().getString("WrongPassword").toLowerCase().equals("tryagain"))
-            {
+        else {
+            if(settings.getConfig().getString("WrongPassword").toLowerCase().equals("tryagain")) {
                 Utils.chat(p, settings.getConfig().getString("PasswordIncorrect"));
             }
-            else
-            {
+            else {
                 p.kickPlayer(ChatColor.translateAlternateColorCodes('&', settings.getConfig().getString("KickMessage")));
             }
         }
