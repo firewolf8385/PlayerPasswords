8dbd50928352e56af0341d2637bc380a2c75907a
colby <colbykrawczyk10@gmail.com>
2022-03-19 01:54:17 -0400

    Some UpdateChecker cleaning

--- src/main/java/com/github/firewolf8385/playerpasswords/UpdateChecker.java
+++ src/main/java/com/github/firewolf8385/playerpasswords/UpdateChecker.java
@@ -9,70 +9,66 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 
-public class UpdateChecker
-{
-    private String currentVersion;
+/**
+ * This class checks if there is a new plugin version available.
+ */
+public class UpdateChecker {
+    private final String currentVersion;
     public static String latestVersion;
     public static boolean update = false;
 
-
-    public UpdateChecker(String currentVersion)
-    {
+    /**
+     * Start the update checker.
+     * @param currentVersion Current plugin version.
+     */
+    public UpdateChecker(String currentVersion) {
         this.currentVersion = currentVersion;
         check.start();
     }
 
-    public boolean updateAvailble()
-    {
-        if(!currentVersion.equals(latestVersion))
-        {
-            return true;
-        }
-
-        return false;
+    /**
+     * Get if there is an update available.
+     * @return Whether or not an update is available.
+     */
+    public boolean updateAvailble() {
+        return !currentVersion.equals(latestVersion);
     }
 
-    private Thread check = new Thread()
-    {
+    /**
+     * Check for a new version in a different thread.
+     */
+    private Thread check = new Thread() {
         @Override
-        public void run()
-        {
+        public void run() {
             URL url = null;
-            try
-            {
+            try {
                 url = new URL("https://api.spigotmc.org/legacy/update.php?resource=70520");
             }
-            catch (MalformedURLException e)
-            {
-                e.printStackTrace();
+            catch (MalformedURLException exception) {
+                exception.printStackTrace();
             }
 
             URLConnection conn = null;
-            try
-            {
+            try {
                 conn = url.openConnection();
             }
-            catch (IOException e)
-            {
-                e.printStackTrace();
+            catch (IOException exception) {
+                exception.printStackTrace();
             }
 
-            try
-            {
+            try {
                 BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
                 latestVersion = reader.readLine();
                 if (!latestVersion.equals(currentVersion)) {
                     update = true;
                     Bukkit.getLogger().info("[PlayerPasswords] There is an update available.\nYour Version: " + currentVersion + "\nLatest Version: " + latestVersion);
                 }
-                else
-                {
+                else {
                     update = false;
                 }
             }
-            catch (IOException e)
-            {
-                e.printStackTrace();
+            catch (IOException exception) {
+                exception.printStackTrace();
             }
         }
     };
