92d9a116a3d36152346e700ed0dff9c1a9102580
colby <colbykrawczyk10@gmail.com>
2023-01-26 23:09:12 -0500

    Prevent the console from using the /password command

--- src/main/java/com/github/firewolf8385/playerpasswords/commands/PasswordCMD.java
+++ src/main/java/com/github/firewolf8385/playerpasswords/commands/PasswordCMD.java
@@ -22,6 +22,12 @@ public class PasswordCMD implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+
+        // Exit if not a player.
+        if(!(sender instanceof Player)) {
+            return true;
+        }
+
         Player player = (Player) sender;
         String uuid = player.getUniqueId().toString();
         PasswordPlayer passwordPlayer = plugin.getPasswordPlayerManager().getPlayer(player);
