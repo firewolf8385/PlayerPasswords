64d707db0cf1a1821f6cefefbd966da943b89d53
firewolf8385 <19617807+firewolf8385@users.noreply.github.com>
2024-01-29 21:10:08 -0500

    Rename PlayerPasswords.java to PlayerPasswordsPlugin.java

--- src/main/java/com/github/firewolf8385/playerpasswords/commands/PasswordCMD.java
+++ src/main/java/com/github/firewolf8385/playerpasswords/commands/PasswordCMD.java
@@ -1,6 +1,30 @@
+/*
+ * This file is part of PlayerPasswords, licensed under the MIT License.
+ *
+ *  Copyright (c) JadedMC
+ *  Copyright (c) contributors
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ */
 package com.github.firewolf8385.playerpasswords.commands;
 
-import com.github.firewolf8385.playerpasswords.PlayerPasswords;
+import com.github.firewolf8385.playerpasswords.PlayerPasswordsPlugin;
 import com.github.firewolf8385.playerpasswords.settings.PluginMessage;
 import com.github.firewolf8385.playerpasswords.settings.SettingsManager;
 import com.github.firewolf8385.playerpasswords.settings.ThemeColor;
@@ -23,13 +47,13 @@ import java.util.List;
  */
 public class PasswordCMD implements CommandExecutor, TabCompleter {
     private final SettingsManager settings = SettingsManager.getInstance();
-    private final PlayerPasswords plugin;
+    private final PlayerPasswordsPlugin plugin;
 
     /**
      * To be able to access the configuration files, we need to pass an instance of the plugin to our listener.
      * @param plugin Instance of the plugin.
      */
-    public PasswordCMD(PlayerPasswords plugin) {
+    public PasswordCMD(PlayerPasswordsPlugin plugin) {
         this.plugin = plugin;
     }
 
--- src/main/java/com/github/firewolf8385/playerpasswords/player/PasswordPlayerManager.java
+++ src/main/java/com/github/firewolf8385/playerpasswords/player/PasswordPlayerManager.java
@@ -1,16 +1,40 @@
+/*
+ * This file is part of PlayerPasswords, licensed under the MIT License.
+ *
+ *  Copyright (c) JadedMC
+ *  Copyright (c) contributors
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ */
 package com.github.firewolf8385.playerpasswords.player;
 
-import com.github.firewolf8385.playerpasswords.PlayerPasswords;
+import com.github.firewolf8385.playerpasswords.PlayerPasswordsPlugin;
 import org.bukkit.entity.Player;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class PasswordPlayerManager {
-    private final PlayerPasswords plugin;
+    private final PlayerPasswordsPlugin plugin;
     private final Map<Player, PasswordPlayer> players = new HashMap<>();
 
-    public PasswordPlayerManager(PlayerPasswords plugin) {
+    public PasswordPlayerManager(PlayerPasswordsPlugin plugin) {
         this.plugin = plugin;
     }
 
