afa566b769171a3f783ecd148746a30d31065502
firewolf8385 <19617807+firewolf8385@users.noreply.github.com>
2024-09-15 03:46:54 -0400

    Allow centering chat messages

--- src/main/java/com/github/firewolf8385/playerpasswords/utils/ChatUtils.java
+++ src/main/java/com/github/firewolf8385/playerpasswords/utils/ChatUtils.java
@@ -29,6 +29,7 @@ import me.clip.placeholderapi.PlaceholderAPI;
 import net.kyori.adventure.platform.bukkit.BukkitAudiences;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.minimessage.MiniMessage;
+import net.md_5.bungee.api.ChatColor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
@@ -40,6 +41,7 @@ import java.util.regex.Pattern;
  * A collection of chat-related utility methods.
  */
 public class ChatUtils {
+    private final static int CENTER_PX = 154;
     private static BukkitAudiences adventure;
     private static PlayerPasswordsPlugin plugin;
 
@@ -62,6 +64,51 @@ public class ChatUtils {
             adventure.close();
             adventure = null;
         }
+    }
+
+    /**
+     * Attempts to center a message in chat.
+     * @param message Message to center.
+     * @return Centered message.
+     */
+    public static String centerText(String message) {
+
+        if(message.equals("")) {
+            return message;
+        }
+
+        String translated = ChatColor.translateAlternateColorCodes('&', MiniMessage.miniMessage().stripTags(toLegacy(message)));
+
+        int messagePxSize = 0;
+        boolean previousCode = false;
+        boolean isBold = false;
+
+        for(char c : translated.toCharArray()) {
+            if(c == '§') {
+                previousCode = true;
+            }
+            else if(previousCode) {
+                previousCode = false;
+                isBold = c == 'l' || c == 'L';
+            }
+            else {
+                DefaultFontInfo dFI = DefaultFontInfo.getDefaultFontInfo(c);
+                messagePxSize += isBold ? dFI.getBoldLength() : dFI.getLength();
+                messagePxSize++;
+            }
+        }
+
+        int halvedMessageSize = messagePxSize / 2;
+        int toCompensate = CENTER_PX - halvedMessageSize;
+        int spaceLength = DefaultFontInfo.SPACE.getLength() + 1;
+        int compensated = 0;
+        StringBuilder sb = new StringBuilder();
+        while(compensated < toCompensate) {
+            sb.append(" ");
+            compensated += spaceLength;
+        }
+
+        return sb + message;
     }
 
     /**
@@ -94,7 +141,13 @@ public class ChatUtils {
      * @param message Message to translate.
      * @return Translated Message.
      */
-    public static Component translate(@NotNull final String message) {
+    public static Component translate(@NotNull String message) {
+
+        // Checks for the "<center>" tag, which centers a message.
+        if(message.startsWith("<center>")) {
+            message = centerText(message.replaceFirst("<center>", ""));
+        }
+
         return MiniMessage.miniMessage().deserialize(replaceLegacy(message));
     }
 
@@ -143,5 +196,129 @@ public class ChatUtils {
                 .replace("&n", "<u>")
                 .replace("&o", "<i>")
                 .replace("&r", "<reset>");
+    }
+
+    /**
+     * Convert a component to its legacy form.
+     * Used because some important plugins don't play nice with MiniMessage.
+     * @param component Component to turn into a legacy string.
+     * @return Resulting legacy string.
+     */
+    public static String toLegacy(Component component) {
+        return MiniMessage.miniMessage().serialize(component).replace("<black>", "§0")
+                .replace("<dark_blue>", "&1")
+                .replace("<dark_green>", "&2")
+                .replace("<dark_aqua>", "&3")
+                .replace("<dark_red>", "&4")
+                .replace("<dark_purple>", "&5")
+                .replace("<gold>", "&6")
+                .replace("<gray>", "&7")
+                .replace("<dark_gray>", "&8")
+                .replace("<blue>", "&9")
+                .replace("<green>", "&a")
+                .replace("<aqua>", "&b")
+                .replace("<red>", "&c")
+                .replace("<light_purple>", "&d")
+                .replace("<yellow>", "&e")
+                .replace("<white>", "&f")
+                .replace("<obfuscated>", "&k")
+                .replace("<obf>", "&k")
+                .replace("<bold>", "&l")
+                .replace("<b>", "&l")
+                .replace("<strikethrough>", "&m")
+                .replace("<st>", "&m")
+                .replace("<underline>", "&n")
+                .replace("<u>", "&n")
+                .replace("<i>", "&o")
+                .replace("<italic>", "&o")
+                .replace("<reset>", "&r")
+                .replace("</black>", "")
+                .replace("</dark_blue>", "")
+                .replace("</dark_green>", "")
+                .replace("</dark_aqua>", "")
+                .replace("</dark_red>", "")
+                .replace("</dark_purple>", "")
+                .replace("</gold>", "")
+                .replace("</gray>", "")
+                .replace("</dark_gray>", "")
+                .replace("</blue>", "")
+                .replace("</green>", "")
+                .replace("</aqua>", "")
+                .replace("</red>", "")
+                .replace("</light_purple>", "")
+                .replace("</yellow>", "")
+                .replace("</white>", "")
+                .replace("</obfuscated>", "")
+                .replace("</obf>", "")
+                .replace("</bold>", "")
+                .replace("</b>", "")
+                .replace("</strikethrough>", "")
+                .replace("</st>", "")
+                .replace("</underline>", "")
+                .replace("</u>", "")
+                .replace("</i>", "")
+                .replace("</italic>", "");
+    }
+
+    /**
+     * Convert a MiniMessage string to its legacy form.
+     * Used because some important plugins don't play nice with MiniMessage.
+     * @param message MiniMessage String to turn into a legacy String.
+     * @return Resulting legacy string.
+     */
+    public static String toLegacy(String message) {
+        return message.replace("<black>", "§0")
+                .replace("<dark_blue>", "&1")
+                .replace("<dark_green>", "&2")
+                .replace("<dark_aqua>", "&3")
+                .replace("<dark_red>", "&4")
+                .replace("<dark_purple>", "&5")
+                .replace("<gold>", "&6")
+                .replace("<gray>", "&7")
+                .replace("<dark_gray>", "&8")
+                .replace("<blue>", "&9")
+                .replace("<green>", "&a")
+                .replace("<aqua>", "&b")
+                .replace("<red>", "&c")
+                .replace("<light_purple>", "&d")
+                .replace("<yellow>", "&e")
+                .replace("<white>", "&f")
+                .replace("<obfuscated>", "&k")
+                .replace("<obf>", "&k")
+                .replace("<bold>", "&l")
+                .replace("<b>", "&l")
+                .replace("<strikethrough>", "&m")
+                .replace("<st>", "&m")
+                .replace("<underline>", "&n")
+                .replace("<u>", "&n")
+                .replace("<i>", "&o")
+                .replace("<italic>", "&o")
+                .replace("<reset>", "&r")
+                .replace("</black>", "")
+                .replace("</dark_blue>", "")
+                .replace("</dark_green>", "")
+                .replace("</dark_aqua>", "")
+                .replace("</dark_red>", "")
+                .replace("</dark_purple>", "")
+                .replace("</gold>", "")
+                .replace("</gray>", "")
+                .replace("</dark_gray>", "")
+                .replace("</blue>", "")
+                .replace("</green>", "")
+                .replace("</aqua>", "")
+                .replace("</red>", "")
+                .replace("</light_purple>", "")
+                .replace("</yellow>", "")
+                .replace("</white>", "")
+                .replace("</obfuscated>", "")
+                .replace("</obf>", "")
+                .replace("</bold>", "")
+                .replace("</b>", "")
+                .replace("</strikethrough>", "")
+                .replace("</st>", "")
+                .replace("</underline>", "")
+                .replace("</u>", "")
+                .replace("</i>", "")
+                .replace("</italic>", "");
     }
 }
